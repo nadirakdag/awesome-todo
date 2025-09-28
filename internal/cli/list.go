3e739d74f34a51516af9b5b16dd954ebced32687
Nadir Akdağ <nadirakdag@outlook.com>
2025-09-29 00:02:48 +0200

    long description fixed

--- internal/cli/list.go
+++ internal/cli/list.go
@@ -9,7 +9,7 @@ import (
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists the tasks",
-	Long:  "Lists the tasks with flags",
+	Long:  "Lists all the tasks with their status",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list of tasks")
 	},
